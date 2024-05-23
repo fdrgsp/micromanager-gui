3d29de33080e28ef474305b84b98e17f09ea6c92
fdrgsp <federico.gasparoli@gmail.com>
2024-05-23 16:49:58 -0400

    fix: reader docstring

--- src/micromanager_gui/_readers/_tensorstore_zarr_reader.py
+++ src/micromanager_gui/_readers/_tensorstore_zarr_reader.py
@@ -8,6 +8,34 @@ import useq
 
 
 class TensorZarrReader:
+    """Read data from a tensorstore zarr file.
+
+    Parameters
+    ----------
+    path : str | Path
+        The path to the tensorstore zarr file.
+
+    Attributes
+    ----------
+    path : Path
+        The path to the tensorstore zarr file.
+    store : ts.TensorStore
+        The tensorstore.
+    metadata : dict
+        The metadata from the acquisition. They are stored in the `.zattrs` file and
+        should contain two keys: `useq_MDASequence` and `useq_MDASequence`.
+    sequence : useq.MDASequence
+        The acquired useq.MDASequence. It is loaded from the metadata using the
+        `useq.MDASequence` key.
+
+    Usage
+    -----
+    reader = TensorZarrReader("path/to/file")
+    # to get the numpy array for a specific axis, for example, the first time point for
+    # the first position and the first z-slice:
+    data = reader.isel({"p": 0, "t": 1, "z": 0})
+    """
+
     def __init__(self, path: str | Path):
         self._path = path
 
