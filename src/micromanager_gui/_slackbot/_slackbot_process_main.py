a1908894c89b84c115dd21afca83a99255d6c830
fdrgsp <federico.gasparoli@gmail.com>
2024-05-29 00:23:14 -0400

    fix: docstrings

--- src/micromanager_gui/_slackbot/_slackbot_process_main.py
+++ src/micromanager_gui/_slackbot/_slackbot_process_main.py
@@ -5,20 +5,17 @@ import sys
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-logging.basicConfig(
-    filename="/Users/fdrgsp/Desktop/process.log",
-    level=logging.INFO,
-)
-
-# this is the main function that will be called when the process is started.
-# the 'handle_message_events' method with the '@app.event("message")' decorator
-# will be called every time a message event is received in the slack channel.
-# the message event is then written to stdout so that in the 'SlackBotProcess'
-# class, the message can be read from the process and emitted as a signal that
-# can be connected to a slot in 'MicroManagerGUI'.
-
 
 def main() -> None:
+    """Main function for the slackbot process.
+
+    This is the main function that will be called when the process is started.
+    The 'handle_message_events' method with the '@app.event("message")' decorator
+    will be called every time a message event is received in the slack channel.
+    The message event is then written to 'stdout' so that in the 'SlackBotProcess'
+    class the message can be read from the process and emitted as a signal that
+    is connected to a slot in 'MicroManagerGUI'.
+    """
     slack_bot_token = sys.argv[1]
     slack_app_token = sys.argv[2]
 
@@ -32,7 +29,6 @@ def main() -> None:
         # write body to stdout
         sys.stdout.write(json.dumps(body))
         sys.stdout.flush()
-        logging.info(f"message: {body}")
 
     handler = SocketModeHandler(app, slack_app_token)
     handler.start()
