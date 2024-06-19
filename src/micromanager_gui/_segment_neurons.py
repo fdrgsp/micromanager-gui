773dcb1f9b825c98e3acb23c599c6ed159e5d779
fdrgsp <federico.gasparoli@gmail.com>
2024-06-19 14:18:28 -0400

    feat: add cellpose

--- src/micromanager_gui/_segment_neurons.py
+++ src/micromanager_gui/_segment_neurons.py
@@ -2,14 +2,22 @@ from __future__ import annotations
 
 import multiprocessing as mp
 from multiprocessing import Process
-from typing import TYPE_CHECKING
+from pathlib import Path
+from typing import TYPE_CHECKING, cast
 
+import tifffile
+import useq
+from cellpose import models
 from pymmcore_plus._logger import logger
+from pymmcore_widgets.useq_widgets._mda_sequence import PYMMCW_METADATA_KEY
 
 if TYPE_CHECKING:
     import numpy as np
-    import useq
     from pymmcore_plus import CMMCorePlus
+
+# use this to select which frame to pick for segmentation. Maybe we can find a way to
+# average multiple frames for better segmentation.
+T_INDEX = 0
 
 
 class SegmentNeurons:
@@ -18,19 +26,23 @@ class SegmentNeurons:
     def __init__(self, mmcore: CMMCorePlus):
         self._mmc = mmcore
 
-        self._is_running: bool = False
+        self._run_cellpose: bool = False
 
         self._segmentation_process: Process | None = None
 
         # Create a multiprocessing Queue
-        self._queue: mp.Queue[tuple[np.ndarray, useq.MDAEvent] | None] = mp.Queue()
+        self._queue: mp.Queue[tuple[np.ndarray, dict] | None] = mp.Queue()
 
         self._mmc.mda.events.sequenceStarted.connect(self._on_sequence_started)
         self._mmc.mda.events.frameReady.connect(self._on_frame_ready)
         self._mmc.mda.events.sequenceFinished.connect(self._on_sequence_finished)
 
     def _on_sequence_started(self, sequence: useq.MDASequence) -> None:
-        self._is_running = True
+        if sequence.metadata.get(PYMMCW_METADATA_KEY, {}).get("should_save", False):
+            self._run_cellpose = True
+        else:
+            self._run_cellpose = False
+            return
 
         # create a separate process for segmentation
         self._segmentation_process = Process(
@@ -42,15 +54,19 @@ class SegmentNeurons:
         self._segmentation_process.start()
 
     def _on_frame_ready(self, image: np.ndarray, event: useq.MDAEvent) -> None:
+        if not self._run_cellpose:
+            return
+
         # if t=0, add the image to the queue
         t_index = event.index.get("t")
-        if t_index is not None and t_index == 0:
+        if t_index is not None and t_index == T_INDEX:
             # send the image to the segmentation process
-            self._queue.put((image, event))
+            self._queue.put((image, event.model_dump()))
             logger.info(f"SegmentNeurons -> Sending image to segment: {event.index}")
 
     def _on_sequence_finished(self, sequence: useq.MDASequence) -> None:
-        self._is_running = False
+        if not self._run_cellpose:
+            return
 
         # stop the segmentation process
         self._queue.put(None)
@@ -58,6 +74,8 @@ class SegmentNeurons:
             self._segmentation_process.join()
         self._segmentation_process = None
         logger.info("SegmentNeurons -> Segmentation worker stopped.")
+
+        self._run_cellpose = False
 
 
 # this must not be part of the SegmentNeurons class
@@ -67,9 +85,47 @@ def _segmentation_worker(queue: mp.Queue) -> None:
         args = queue.get()
         if args is None:
             break
-        _segment_image(*args)
+        image, event = args
+        _segment_image(image, event)
 
 
-def _segment_image(image: np.ndarray, event: useq.MDAEvent) -> None:
+def _segment_image(image: np.ndarray, event: dict) -> None:
     """Segment the image."""
     logger.info(f"SegmentNeurons -> Segmenting image: {image.shape}")
+
+    useq_event = useq.MDAEvent(**event)
+    seq = useq_event.sequence
+    if seq is None:
+        return
+
+    # get position index
+    p_idx = useq_event.index.get("p", 0)
+
+    # get the metadata from the sequence
+    seq_meta = cast(dict, seq.metadata.get(PYMMCW_METADATA_KEY, {}))
+
+    # get the save path
+    save_path = seq_meta.get("save_dir", "")
+
+    # create the labels save name
+    save_name = seq_meta.get("save_name", "")
+
+    # create a labels folder if it does not exist
+    save_path = Path(save_path) / f"{save_name}_labels"
+    save_path.mkdir(parents=True, exist_ok=True)
+
+    # create the labels file name
+    label_name = f"{save_name}_labels_{useq_event.pos_name}_p{p_idx}.tif"
+
+    # set the cellpose model and parameters
+    model_type = "cyto3"  # or CellposeModel(custom_model_path)
+    channel = [0, 0]
+    diameter = 0
+    model = models.Cellpose(gpu=True, model_type=model_type)
+
+    # run cellpose
+    masks, _, _, _ = model.eval(image, diameter=diameter, channels=channel)
+
+    # save to disk
+    tifffile.imsave(save_path / label_name, masks)
+    logger.info(f"SegmentNeurons -> Saving labels: {save_path}/{label_name}")
