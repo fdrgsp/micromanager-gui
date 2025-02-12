4e0e1799b6af2fc90e54c7d1646e53d0827a674c
Anna Yang <annayangzq26@gmail.com>
2025-02-12 09:12:27 -0500

    to get training images

--- src/micromanager_gui/_plate_viewer/trainingImage.py
+++ src/micromanager_gui/_plate_viewer/trainingImage.py
@@ -3,20 +3,26 @@ from pathlib import Path
 
 import tifffile
 
+# from _plate_viewer._plate_viewer import PlateViewer
 from micromanager_gui.readers import TensorstoreZarrReader
-from ._plate_viewer import PlateViewer
 
 path = r'/Volumes/T7 Shield/LAM77_NC240503_384_CBD_20240927/NC240503_240927_Treated_CBDanalogs.tensorstore.zarr'
 
 img_path = Path(path).stem
 
-save_path = '/Users/annayang26/Desktop/CellposeTraining/' + f'{img_path}.tif'
-
 ts = TensorstoreZarrReader(path)
 
-pos = random.randint(0, 10)
+total_pos = int(ts.isel().shape[0])
+print(total_pos)
+
+pos = random.randint(0, total_pos-1)
 data = ts.isel(p=pos)
 
 data_half_to_end = data[data.shape[0] // 2 :, :, :]
+cyto_frame = data_half_to_end.max(axis=0)
 
-tifffile.imwrite(save_path, data_half_to_end, imagej=True)
+train_save_path = '/Users/annayang26/Desktop/CellposeTraining/' + f'{img_path}_{pos}.tif'
+tifffile.imwrite(train_save_path, cyto_frame, imagej=True)
+
+img_save_path = '/Users/annayang26/Desktop/CellposeTraining/image/' + f'{img_path}_{pos}.tif'
+tifffile.imwrite(img_save_path, data_half_to_end, imagej=True)
