4dbb64def128c7174a18ed77430ed74b6f38451f
fdrgsp <federico.gasparoli@gmail.com>
2025-03-05 23:39:46 -0500

    update plots

--- src/micromanager_gui/_plate_viewer/_plot_methods.py
+++ src/micromanager_gui/_plate_viewer/_plot_methods.py
@@ -266,12 +266,18 @@ def _plot_multi_well_data(
     widget.figure.clear()
     ax = widget.figure.add_subplot(111)
 
+    well_count: int = 0
+    wells_names: list[str] = []
     # loop over the data and plot the data
-    for well_and_pos, roi_data in data.items():
-        if positions is not None and int(well_and_pos) not in positions:
+    for well_and_fov, roi_data in data.items():
+        if positions is not None and int(well_and_fov) not in positions:
             continue
 
-        pos = int(well_and_pos.split("_")[1])
+        # this is to store the well names to then use them as x axis labels
+        if (well := well_and_fov.split("_")[0]) not in wells_names:
+            wells_names.append(well)
+            # we increment the well count only when we find a new well
+            well_count += 1
 
         for roi in roi_data:
             r_data = roi_data[roi]
@@ -282,7 +288,7 @@ def _plot_multi_well_data(
                 amp_list = r_data.peaks_amplitudes_dec_dff
                 well_freq_list = [r_data.dec_dff_frequency] * len(amp_list)
                 ax.plot(
-                    amp_list, well_freq_list, "o", label=f"{well_and_pos} - roi{roi}"
+                    amp_list, well_freq_list, "o", label=f"{well_and_fov} - roi{roi}"
                 )
                 ax.set_xlabel("Amplitude")
                 ax.set_ylabel("Frequency")
@@ -291,35 +297,40 @@ def _plot_multi_well_data(
                 if r_data.peaks_amplitudes_dec_dff is None:
                     continue
                 ax.plot(
-                    [pos] * len(r_data.peaks_amplitudes_dec_dff),
+                    [well_count] * len(r_data.peaks_amplitudes_dec_dff),
                     r_data.peaks_amplitudes_dec_dff,
                     "o",
-                    label=f"{well_and_pos} - roi{roi}",
+                    label=f"{well_and_fov} - roi{roi}",
                 )
-                ax.set_xlabel("Positions")
+                ax.set_xlabel("Wells")
                 ax.set_ylabel("Amplitude")
 
             elif freq:
                 ax.plot(
-                    pos,
+                    well_count,
                     r_data.dec_dff_frequency,
                     "o",
-                    label=f"{well_and_pos} - roi{roi}",
+                    label=f"{well_and_fov} - roi{roi}",
                 )
-                ax.set_xlabel("Positions")
+                ax.set_xlabel("Wells")
                 ax.set_ylabel("Frequency")
 
             elif iei:
                 if r_data.iei is None:
                     continue
                 ax.plot(
-                    [pos] * len(r_data.iei),
+                    [well_count] * len(r_data.iei),
                     r_data.iei,
                     "o",
-                    label=f"{well_and_pos} - roi{roi}",
+                    label=f"{well_and_fov} - roi{roi}",
                 )
-                ax.set_xlabel("Positions")
+                ax.set_xlabel("Wells")
                 ax.set_ylabel("Inter-event intervals (sec)")
+
+    # set ticks labels as well names
+    if (amp or freq or iei) and not (amp and freq):
+        ax.set_xticks(range(1, len(wells_names) + 1))
+        ax.set_xticklabels(wells_names)
 
     widget.figure.tight_layout()
 
