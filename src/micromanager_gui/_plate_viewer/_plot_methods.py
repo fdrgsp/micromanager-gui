0eeab4bd643208a826eb5645047326f1174fc969
fdrgsp <federico.gasparoli@gmail.com>
2025-03-07 14:45:43 -0500

    update raster with different colors

--- src/micromanager_gui/_plate_viewer/_plot_methods.py
+++ src/micromanager_gui/_plate_viewer/_plot_methods.py
@@ -2,8 +2,10 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, cast
 
+import matplotlib.cm as cm
 import mplcursors
 import numpy as np
+from matplotlib.colors import Normalize
 
 from ._util import (
     DEC_DFF,
@@ -18,10 +20,13 @@ from ._util import (
     DFF_NORMALIZED,
     NORMALIZED_TRACES,
     RASTER_PLOT,
+    RASTER_PLOT_AMP,
     RAW_TRACES,
 )
 
 if TYPE_CHECKING:
+    from matplotlib.axes import Axes
+
     from ._graph_widgets import _MultilWellGraphWidget, _SingleWellGraphWidget
     from ._util import ROIData
 
@@ -39,7 +44,8 @@ SINGLE_WELL_GRAPHS_OPTIONS: dict[str, dict[str, bool]] = {
     DEC_DFF_AMPLITUDE: {"dec": True, "amp": True},
     DEC_DFF_FREQUENCY: {"dec": True, "freq": True},
     DEC_DFF_AMPLITUDE_VS_FREQUENCY: {"dec": True, "amp": True, "freq": True},
-    RASTER_PLOT: {"raster": True},
+    RASTER_PLOT: {"amplitude_colors": False},
+    RASTER_PLOT_AMP: {"amplitude_colors": True},
     DEC_DFF_IEI: {"dec": True, "iei": True},
 }
 
@@ -70,11 +76,114 @@ def plot_single_well_traces(
     if not text or text == "None":
         return
 
-    # get the options for the text using the SINGLE_WELL_GRAPHS_OPTIONS dictionary that
-    # maps the text to the options
-    return _plot_single_well_traces(
-        widget, data, rois, **SINGLE_WELL_GRAPHS_OPTIONS[text]
-    )
+    # get the options for the text using the SINGLE_WELL_GRAPHS_OPTIONS dictionary
+    # that maps the text to the options
+
+    # plot raster plot
+    if text in {RASTER_PLOT, RASTER_PLOT_AMP}:
+        return _generate_raster_plot(
+            widget, data, rois, **SINGLE_WELL_GRAPHS_OPTIONS[text]
+        )
+
+    # plot other types of graphs
+    else:
+        return _plot_single_well_traces(
+            widget, data, rois, **SINGLE_WELL_GRAPHS_OPTIONS[text]
+        )
+
+
+def _generate_raster_plot(
+    widget: _SingleWellGraphWidget,
+    data: dict,
+    rois: list[int] | None = None,
+    amplitude_colors: bool = False,
+) -> None:
+    """Generate a raster plot."""
+    # Clear the figure
+    widget.figure.clear()
+    ax = widget.figure.add_subplot(111)
+
+    rois_rec_time: list[float] = []
+    total_frames: int = 0
+    trace: list[float] | None = None
+
+    # create the color palette for the raster plot
+    if amplitude_colors:
+        # calculate the min and max amplitudes color range
+        min_amp: float = 0
+        max_amp: float = 0
+        for roi_key in data:
+            if rois is not None and int(roi_key) not in rois:
+                continue
+            roi_data = cast("ROIData", data[roi_key])
+            if roi_data.peaks_amplitudes_dec_dff is None:
+                continue
+            roi_min_amp = min(roi_data.peaks_amplitudes_dec_dff)
+            roi_max_amp = max(roi_data.peaks_amplitudes_dec_dff)
+            min_amp = min(min_amp, roi_min_amp)
+            max_amp = max(max_amp, roi_max_amp)
+        # adding 0.5 to max_amp to make the color range more visible
+        norm_amp_color = Normalize(vmin=min_amp, vmax=max_amp * 0.5)
+    else:
+        colors_list = [f"C{i}" for i in range(len(data))]
+
+    # loop over the ROIData and get the peaks and their colors for each ROI
+    event_data: list[list[float]] = []
+    colors: list[list[str]] = []
+    for roi_key in data:
+        if rois is not None and int(roi_key) not in rois:
+            continue
+
+        roi_data = cast("ROIData", data[roi_key])
+
+        if roi_data.peaks_dec_dff is None or roi_data.peaks_amplitudes_dec_dff is None:
+            continue
+
+        # this is to then convert thx frames on x axis to seconds
+        if (ttime := roi_data.total_recording_time_in_sec) is not None:
+            rois_rec_time.append(ttime)
+        # assuming all traces have the same number of frames
+        if not total_frames and (trace := roi_data.raw_trace) is not None:
+            total_frames = len(trace)
+
+        event_data.append(roi_data.peaks_dec_dff)
+        if amplitude_colors:
+            peak_colors = [
+                cm.viridis(norm_amp_color(amp))
+                for amp in roi_data.peaks_amplitudes_dec_dff
+            ]
+            colors.append(peak_colors)
+        else:
+            colors.append([colors_list[int(roi_key) - 1]] * len(roi_data.peaks_dec_dff))
+
+    # plot the raster plot
+    ax.eventplot(event_data, colors=colors)
+
+    # set the axis labels
+    ax.set_ylabel("ROIs")
+    # hide the y-axis labels and ticks
+    ax.set_yticklabels([])
+    ax.set_yticks([])
+    update_time_axis(ax, rois_rec_time, trace)
+
+    widget.figure.tight_layout()
+
+    # Add hover functionality using mplcursors
+    cursor = mplcursors.cursor(ax, hover=mplcursors.HoverMode.Transient)
+
+    @cursor.connect("add")  # type: ignore [misc]
+    def on_add(sel: mplcursors.Selection) -> None:
+        sel.annotation.set(text=sel.artist.get_label(), fontsize=8, color="black")
+        # emit the graph widget roiSelected signal
+        if sel.artist.get_label():
+            # sel.artist.get_label() rentrns a _child0
+            child = int(sel.artist.get_label()[6:])
+            roi = str(rois[child]) if rois is not None else str(child + 1)
+            sel.annotation.set(text=f"ROI {roi}", fontsize=8, color="black")
+            if roi.isdigit():
+                widget.roiSelected.emit(roi)
+
+    widget.canvas.draw()
 
 
 def _plot_single_well_traces(
@@ -87,7 +196,6 @@ def _plot_single_well_traces(
     with_peaks: bool = False,
     amp: bool = False,
     freq: bool = False,
-    raster: bool = False,
     iei: bool = False,
 ) -> None:
     """Plot various types of traces."""
@@ -107,20 +215,13 @@ def _plot_single_well_traces(
     # loop over the ROIData and plot the traces per ROI
     count = 0
 
-    # for raster plot
-    spikes: list[list[int]] = []
-    roi_to_draw: list[str] = []
-    colors_to_add: list[str] = []
-
-    colors = [f"C{i}" for i in range(len(data))]
-
     rois_rec_time: list[float] = []
 
-    for key in data:
-        if rois is not None and int(key) not in rois:
+    for roi_key in data:
+        if rois is not None and int(roi_key) not in rois:
             continue
 
-        roi_data = cast("ROIData", data[key])
+        roi_data = cast("ROIData", data[roi_key])
 
         # get the correct trace based on the flags
         trace = get_trace(roi_data, dff, dec)
@@ -136,53 +237,47 @@ def _plot_single_well_traces(
                 continue
             amp_list = roi_data.peaks_amplitudes_dec_dff
             roi_freq_list = [roi_data.dec_dff_frequency] * len(amp_list)
-            ax.plot(amp_list, roi_freq_list, "o", label=f"ROI {key}")
+            ax.plot(amp_list, roi_freq_list, "o", label=f"ROI {roi_key}")
 
         elif amp:
             # plot amplitude
             if roi_data.peaks_amplitudes_dec_dff is None:
                 continue
             ax.plot(
-                [int(key)] * len(roi_data.peaks_amplitudes_dec_dff),
+                [int(roi_key)] * len(roi_data.peaks_amplitudes_dec_dff),
                 roi_data.peaks_amplitudes_dec_dff,
                 "o",
-                label=f"ROI {key}",
+                label=f"ROI {roi_key}",
             )
 
         elif freq:
             # plot frequency
-            ax.plot(int(key), roi_data.dec_dff_frequency, "o", label=f"ROI {key}")
-
-        elif raster:
-            if roi_data.peaks_dec_dff is None:
-                continue
-
-            peaks_indices = [int(peak_ind) for peak_ind in roi_data.peaks_dec_dff]
-            spikes.append(peaks_indices)
-            roi_to_draw.append(key)
-            colors_to_add.append(colors[int(key) - 1])
-            ax.set_xlabel("Frames")
-            ax.set_ylabel("ROIs")
+            ax.plot(
+                int(roi_key), roi_data.dec_dff_frequency, "o", label=f"ROI {roi_key}"
+            )
 
         elif iei:
             # plot inter-event intervals
             if roi_data.iei is None:
                 continue
             ax.plot(
-                [int(key)] * len(roi_data.iei), roi_data.iei, "o", label=f"ROI {key}"
+                [int(roi_key)] * len(roi_data.iei),
+                roi_data.iei,
+                "o",
+                label=f"ROI {roi_key}",
             )
 
         else:
             # normalize if the flag is set
             if normalize:
                 trace = normalize_trace(trace)
-                ax.plot(np.array(trace) + count, label=f"ROI {key}")
+                ax.plot(np.array(trace) + count, label=f"ROI {roi_key}")
                 # hide the y-axis labels
                 ax.set_yticklabels([])
                 # hide the ticks
                 ax.set_yticks([])
             else:
-                ax.plot(trace, label=f"ROI {key}")
+                ax.plot(trace, label=f"ROI {roi_key}")
 
             # plot the peaks if the flag is set
             if with_peaks:
@@ -193,14 +288,10 @@ def _plot_single_well_traces(
                     peaks_indices,
                     np.array(trace)[peaks_indices] + (count if normalize else 0),
                     "x",
-                    label=f"Peaks ROI {key}",
+                    label=f"Peaks ROI {roi_key}",
                 )
 
         count += COUNT_INCREMENT
-
-    if raster:
-        ax.eventplot(spikes, colors=colors_to_add)
-        # ax.set_yticklabels(roi_to_draw)
 
     # set the axis labels
     if amp and freq:
@@ -220,25 +311,10 @@ def _plot_single_well_traces(
             ax.set_ylabel("dF/F")
         elif dec:
             ax.set_ylabel("Deconvolved dF/F")
-        elif raster:
-            ax.set_ylabel("ROIs")
         else:
             ax.set_ylabel("Fluorescence Intensity")
-
-        if sum(rois_rec_time) > 0:
-            # get the average total recording time in seconds
-            avg_rec_time = int(np.mean(rois_rec_time))
-            # get total number of frames from last trace (they should all be the same)
-            total_frames = len(trace) if trace is not None else 1
-            # compute tick positions
-            tick_interval = avg_rec_time / total_frames
-            x_ticks = np.linspace(0, total_frames, num=5, dtype=int)
-            x_labels = [str(int(t * tick_interval)) for t in x_ticks]
-            ax.set_xticks(x_ticks)
-            ax.set_xticklabels(x_labels)
-            ax.set_xlabel("Time (s)")
-        else:
-            ax.set_xlabel("Frames")
+        # update the x-axis to show time in seconds or frames
+        update_time_axis(ax, rois_rec_time, trace)
 
     widget.figure.tight_layout()
 
@@ -250,17 +326,30 @@ def _plot_single_well_traces(
         sel.annotation.set(text=sel.artist.get_label(), fontsize=8, color="black")
         # emit the graph widget roiSelected signal
         if sel.artist.get_label():
-            if raster:
-                label_list = [num for num in sel.artist.get_label() if num.isdigit()]
-                index = int("".join(label_list))
-                roi = str(roi_to_draw[index])
-                sel.annotation.set(text=f"ROI {roi}", fontsize=8, color="black")
-            else:
-                roi = cast(str, sel.artist.get_label().split(" ")[1])
-                if roi.isdigit():
-                    widget.roiSelected.emit(roi)
+            roi = cast(str, sel.artist.get_label().split(" ")[1])
+            if roi.isdigit():
+                widget.roiSelected.emit(roi)
 
     widget.canvas.draw()
+
+
+def update_time_axis(
+    ax: Axes, rois_rec_time: list[float], trace: list[float] | None
+) -> None:
+    if trace is None or sum(rois_rec_time) <= 0:
+        ax.set_xlabel("Frames")
+        return
+    # get the average total recording time in seconds
+    avg_rec_time = int(np.mean(rois_rec_time))
+    # get total number of frames from last trace (they should all be the same)
+    total_frames = len(trace) if trace is not None else 1
+    # compute tick positions
+    tick_interval = avg_rec_time / total_frames
+    x_ticks = np.linspace(0, total_frames, num=5, dtype=int)
+    x_labels = [str(int(t * tick_interval)) for t in x_ticks]
+    ax.set_xticks(x_ticks)
+    ax.set_xticklabels(x_labels)
+    ax.set_xlabel("Time (s)")
 
 
 def get_trace(roi_data: ROIData, dff: bool, dec: bool) -> list[float] | None:
