d5a9d2c02bbfa98318b7071c9efb01f574bde351
fdrgsp <federico.gasparoli@gmail.com>
2025-03-09 12:42:34 -0400

    fix raster

--- src/micromanager_gui/_plate_viewer/_plot_methods.py
+++ src/micromanager_gui/_plate_viewer/_plot_methods.py
@@ -109,6 +109,131 @@ def plot_single_well_traces(
         )
 
 
+def visualize_stimulated_area(
+    widget: _SingleWellGraphWidget,
+    data: dict,
+    rois: list[int] | None = None,
+    with_rois: bool = False,
+) -> None:
+    """Visualize Stimulated area."""
+    # Clear the figure
+    widget.figure.clear()
+    ax = widget.figure.add_subplot(111)
+
+    # get analysis path
+    analysis_path = widget._plate_viewer.analysis_files_path
+    if analysis_path is None:
+        return
+
+    # get the stimulation mask
+    stimulation_mask_path = Path(analysis_path) / STIMULATION_MASK
+    if not stimulation_mask_path.exists():
+        return
+    stim_mask = tifffile.imread(stimulation_mask_path)
+
+    color_mapping = {0: "black", 1: "white"}  # 0: background, 1: stimulated area
+
+    if with_rois:
+        # get the labels file path
+        labels_image_path = widget._plate_viewer.labels_path
+        if labels_image_path is None:
+            return
+
+        stim, non_stim = _group_rois(data, rois)
+
+        # open label image
+        r = str(rois[0]) if rois is not None else "1"
+        label_name = cast("ROIData", data[r]).well_fov_position
+        if not label_name:
+            return
+        labels = tifffile.imread(Path(labels_image_path) / label_name)
+
+        # create a color mapping for the labels
+        labels_range = np.unique(labels[labels != 0])
+        for roi in labels_range:
+            if roi in stim:
+                color_mapping[roi] = STIMULATED_COLOR
+            elif roi in non_stim:
+                color_mapping[roi] = NON_STIMULATED_COLOR
+            else:
+                color_mapping[roi] = DEFAULT_COLOR
+
+        # plot the labels image with the color mapping
+        unique_labels = np.unique(labels)
+        colors = [color_mapping.get(lbl, DEFAULT_COLOR) for lbl in unique_labels]
+        cmap = ListedColormap(colors)
+        norm = BoundaryNorm(
+            boundaries=np.append(unique_labels, unique_labels[-1] + 1),
+            ncolors=len(colors),
+        )
+        ax.imshow(labels, cmap=cmap, norm=norm)
+
+        # create and add legend
+        legend_patches = [
+            Patch(color="green", label="Stimulated ROIs"),
+            Patch(color="magenta", label="Non-Stimulated ROIs"),
+        ]
+        ax.legend(
+            handles=legend_patches,
+            loc="lower center",
+            bbox_to_anchor=(0.5, 1.02),  # moves it above the plot (x, y)
+            ncol=2,  # single row
+            frameon=True,
+            fontsize="small",
+            edgecolor="black",
+        )
+
+    else:
+        ax.imshow(stim_mask, cmap="gray")
+
+    ax.axis("off")
+
+    widget.figure.tight_layout()
+
+    if with_rois:
+        # Add hover functionality using mplcursors
+        cursor = mplcursors.cursor(ax, hover=mplcursors.HoverMode.Transient)
+
+        @cursor.connect("add")  # type: ignore [misc]
+        def on_add(sel: mplcursors.Selection) -> None:
+            roi_val = None
+            # emit the graph widget roiSelected signal
+            x, y = int(sel.target[0]), int(sel.target[1])
+            if 0 <= y < stim_mask.shape[0] and 0 <= x < stim_mask.shape[1]:
+                roi_val = str(labels[y, x]) if labels[y, x] > 0 else None
+            if roi_val:
+                sel.annotation.set(text=f"ROI {roi_val}", fontsize=8, color="yellow")
+                sel.annotation.arrow_patch.set_color("yellow")
+                sel.annotation.arrow_patch.set_alpha(1)  # arrow is visible
+            else:
+                sel.annotation.set_visible(False)  # hide annotation
+                sel.annotation.arrow_patch.set_alpha(0)  # hide arrow
+            # emit the graph widget roiSelected signal
+            if roi_val and roi_val.isdigit():
+                widget.roiSelected.emit(roi_val)
+
+    widget.canvas.draw()
+
+
+def _group_rois(data: dict, rois: list[int] | None) -> tuple[list[int], list[int]]:
+    """To group the ROIs based on stimulated state."""
+    stimulated_rois: list[int] = []
+    non_stimulated_rois: list[int] = []
+
+    for roi_key in data:
+        if rois is not None and int(roi_key) not in rois:
+            continue
+
+        roi_data = cast("ROIData", data[roi_key])
+
+        if roi_data.stimulated:
+            stimulated_rois.append(int(roi_key))
+        else:
+            non_stimulated_rois.append(int(roi_key))
+
+    return stimulated_rois, non_stimulated_rois
+
+
 def _generate_raster_plot(
     widget: _SingleWellGraphWidget,
     data: dict,
@@ -142,11 +267,12 @@ def _generate_raster_plot(
 
         roi_data = cast("ROIData", roi_data)
 
-        if not roi_data.peaks_dec_dff or not roi_data.peaks_amplitudes_dec_dff:
+        if roi_data.peaks_dec_dff is None or roi_data.peaks_amplitudes_dec_dff is None:
             continue
 
         # this is to then convert the x-axis frames to seconds
         if roi_data.total_recording_time_in_sec is not None:
+            print(roi_key, "appending rec time")
             rois_rec_time.append(roi_data.total_recording_time_in_sec)
 
         # assuming all traces have the same number of frames
@@ -190,6 +316,9 @@ def _generate_raster_plot(
     # hide the y-axis labels and ticks
     ax.set_yticklabels([])
     ax.set_yticks([])
+
+    # use the last trace to get total number of frames (they should all be the same)
+    trace = roi_data.raw_trace
     update_time_axis(ax, rois_rec_time, trace)
 
     # add the colorbar if amplitude colors are used
@@ -215,6 +344,26 @@ def _generate_raster_plot(
                 widget.roiSelected.emit(roi)
 
     widget.canvas.draw()
+
+
+def update_time_axis(
+    ax: Axes, rois_rec_time: list[float], trace: list[float] | None
+) -> None:
+    print("updating time axis", trace is not None, sum(rois_rec_time) > 0)
+    if trace is None or sum(rois_rec_time) <= 0:
+        ax.set_xlabel("Frames")
+        return
+    # get the average total recording time in seconds
+    avg_rec_time = int(np.mean(rois_rec_time))
+    # get total number of frames from the trace
+    total_frames = len(trace) if trace is not None else 1
+    # compute tick positions
+    tick_interval = avg_rec_time / total_frames
+    x_ticks = np.linspace(0, total_frames, num=5, dtype=int)
+    x_labels = [str(int(t * tick_interval)) for t in x_ticks]
+    ax.set_xticks(x_ticks)
+    ax.set_xticklabels(x_labels)
+    ax.set_xlabel("Time (s)")
 
 
 def _plot_single_well_traces(
@@ -364,25 +513,6 @@ def _plot_single_well_traces(
     widget.canvas.draw()
 
 
-def update_time_axis(
-    ax: Axes, rois_rec_time: list[float], trace: list[float] | None
-) -> None:
-    if trace is None or sum(rois_rec_time) <= 0:
-        ax.set_xlabel("Frames")
-        return
-    # get the average total recording time in seconds
-    avg_rec_time = int(np.mean(rois_rec_time))
-    # get total number of frames from last trace (they should all be the same)
-    total_frames = len(trace) if trace is not None else 1
-    # compute tick positions
-    tick_interval = avg_rec_time / total_frames
-    x_ticks = np.linspace(0, total_frames, num=5, dtype=int)
-    x_labels = [str(int(t * tick_interval)) for t in x_ticks]
-    ax.set_xticks(x_ticks)
-    ax.set_xticklabels(x_labels)
-    ax.set_xlabel("Time (s)")
-
-
 def get_trace(roi_data: ROIData, dff: bool, dec: bool) -> list[float] | None:
     """Get the appropriate trace based on the flags."""
     if dff and dec:
@@ -399,131 +529,6 @@ def normalize_trace(trace: list[float]) -> list[float]:
     tr = np.array(trace)
     normalized = (tr - np.min(tr)) / (np.max(tr) - np.min(tr))
     return cast(list[float], normalized.tolist())
-
-
-def visualize_stimulated_area(
-    widget: _SingleWellGraphWidget,
-    data: dict,
-    rois: list[int] | None = None,
-    with_rois: bool = False,
-) -> None:
-    """Visualize Stimulated area."""
-    # Clear the figure
-    widget.figure.clear()
-    ax = widget.figure.add_subplot(111)
-
-    # get analysis path
-    analysis_path = widget._plate_viewer.analysis_files_path
-    if analysis_path is None:
-        return
-
-    # get the stimulation mask
-    stimulation_mask_path = Path(analysis_path) / STIMULATION_MASK
-    if not stimulation_mask_path.exists():
-        return
-    stim_mask = tifffile.imread(stimulation_mask_path)
-
-    color_mapping = {0: "black", 1: "white"}  # 0: background, 1: stimulated area
-
-    if with_rois:
-        # get the labels file path
-        labels_image_path = widget._plate_viewer.labels_path
-        if labels_image_path is None:
-            return
-
-        stim, non_stim = _group_rois(data, rois)
-
-        # open label image
-        r = str(rois[0]) if rois is not None else "1"
-        label_name = cast("ROIData", data[r]).well_fov_position
-        if not label_name:
-            return
-        labels = tifffile.imread(Path(labels_image_path) / label_name)
-
-        # create a color mapping for the labels
-        labels_range = np.unique(labels[labels != 0])
-        for roi in labels_range:
-            if roi in stim:
-                color_mapping[roi] = STIMULATED_COLOR
-            elif roi in non_stim:
-                color_mapping[roi] = NON_STIMULATED_COLOR
-            else:
-                color_mapping[roi] = DEFAULT_COLOR
-
-        # plot the labels image with the color mapping
-        unique_labels = np.unique(labels)
-        colors = [color_mapping.get(lbl, DEFAULT_COLOR) for lbl in unique_labels]
-        cmap = ListedColormap(colors)
-        norm = BoundaryNorm(
-            boundaries=np.append(unique_labels, unique_labels[-1] + 1),
-            ncolors=len(colors),
-        )
-        ax.imshow(labels, cmap=cmap, norm=norm)
-
-        # create and add legend
-        legend_patches = [
-            Patch(color="green", label="Stimulated ROIs"),
-            Patch(color="magenta", label="Non-Stimulated ROIs"),
-        ]
-        ax.legend(
-            handles=legend_patches,
-            loc="lower center",
-            bbox_to_anchor=(0.5, 1.02),  # moves it above the plot (x, y)
-            ncol=2,  # single row
-            frameon=True,
-            fontsize="small",
-            edgecolor="black",
-        )
-
-    else:
-        ax.imshow(stim_mask, cmap="gray")
-
-    ax.axis("off")
-
-    widget.figure.tight_layout()
-
-    if with_rois:
-        # Add hover functionality using mplcursors
-        cursor = mplcursors.cursor(ax, hover=mplcursors.HoverMode.Transient)
-
-        @cursor.connect("add")  # type: ignore [misc]
-        def on_add(sel: mplcursors.Selection) -> None:
-            roi_val = None
-            # emit the graph widget roiSelected signal
-            x, y = int(sel.target[0]), int(sel.target[1])
-            if 0 <= y < stim_mask.shape[0] and 0 <= x < stim_mask.shape[1]:
-                roi_val = str(labels[y, x]) if labels[y, x] > 0 else None
-            if roi_val:
-                sel.annotation.set(text=f"ROI {roi_val}", fontsize=8, color="yellow")
-                sel.annotation.arrow_patch.set_color("yellow")
-                sel.annotation.arrow_patch.set_alpha(1)  # arrow is visible
-            else:
-                sel.annotation.set_visible(False)  # hide annotation
-                sel.annotation.arrow_patch.set_alpha(0)  # hide arrow
-            # emit the graph widget roiSelected signal
-            if roi_val and roi_val.isdigit():
-                widget.roiSelected.emit(roi_val)
-
-    widget.canvas.draw()
-
-
-def _group_rois(data: dict, rois: list[int] | None) -> tuple[list[int], list[int]]:
-    """To group the ROIs based on stimulated state."""
-    stimulated_rois: list[int] = []
-    non_stimulated_rois: list[int] = []
-
-    for roi_key in data:
-        if rois is not None and int(roi_key) not in rois:
-            continue
-
-        roi_data = cast("ROIData", data[roi_key])
-
-        if roi_data.stimulated:
-            stimulated_rois.append(int(roi_key))
-        else:
-            non_stimulated_rois.append(int(roi_key))
-
-    return stimulated_rois, non_stimulated_rois
 
 
 # ------------------------------MULTI-WELL PLOTTING-------------------------------------
